fc71b8a670b12e3604acebdac6cb2370b0af90c0
João Victor <106018733+joaovict18@users.noreply.github.com>
2023-07-19 08:47:46 -0300

    progresso

---
+++ variaveis_e_desvios_condicionais/08.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+
+    char gen;
+    
+    printf("Digite seu sexo: ");
+    scanf("%c", &gen)
+
+    if (gen == "M") {
+
+
+    } else {
+        print("Genero invalido")
+    }
+}
