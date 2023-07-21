fc9f4187274d041b90d8cff49330516d714b359d
Ed1403 <139581062+Ed1403@users.noreply.github.com>
2023-07-21 07:34:36 -0300

    att10

---
+++ variaveis_e_desvios_condicionais/10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () {
+	
+	// quantidade, preço total
+	int quant;
+	float pt;
+	
+	printf ("Digite a quantidade de unidades: ");
+	scanf ("%d", &quant);
+	
+	if (quant < 5) {
+		pt = quant * 3;
+	} else {
+		pt = quant * 2,50;
+	}
+	
+	printf ("O valor total é %.2f", pt);
+	
+	return 0;
+}
